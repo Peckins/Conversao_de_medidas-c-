08765d09773e2bba324c04230871ad795d56e694
Peckins <messias.morais@etec.sp.gov.br>
2019-03-20 17:05:13 -0300

    Primeira entrega do projeto

---
+++ exercicioMedidas/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace exercicioMedidas
+{
+    partial class Calculador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_res = new System.Windows.Forms.TextBox();
+            this.txt_Valor1 = new System.Windows.Forms.TextBox();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_Calc = new System.Windows.Forms.Button();
+            this.rbtn3 = new System.Windows.Forms.RadioButton();
+            this.rbtn2 = new System.Windows.Forms.RadioButton();
+            this.rbtn1 = new System.Windows.Forms.RadioButton();
+            this.gBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rbtn6 = new System.Windows.Forms.RadioButton();
+            this.rbtn5 = new System.Windows.Forms.RadioButton();
+            this.rbtn4 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gBox1.SuspendLayout();
+            this.gBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gBox1
+            // 
+            this.gBox1.BackColor = System.Drawing.Color.White;
+            this.gBox1.Controls.Add(this.label4);
+            this.gBox1.Controls.Add(this.label1);
+            this.gBox1.Controls.Add(this.txt_res);
+            this.gBox1.Controls.Add(this.txt_Valor1);
+            this.gBox1.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gBox1.Location = new System.Drawing.Point(9, 28);
+            this.gBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gBox1.Name = "gBox1";
+            this.gBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gBox1.Size = new System.Drawing.Size(218, 210);
+            this.gBox1.TabIndex = 0;
+            this.gBox1.TabStop = false;
+            this.gBox1.Text = "Insira os valores";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(68, 32);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Seu valor";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 117);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Resultado";
+            // 
+            // txt_res
+            // 
+            this.txt_res.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_res.Location = new System.Drawing.Point(20, 137);
+            this.txt_res.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_res.Name = "txt_res";
+            this.txt_res.Size = new System.Drawing.Size(163, 27);
+            this.txt_res.TabIndex = 2;
+            this.txt_res.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txt_Valor1
+            // 
+            this.txt_Valor1.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Valor1.Location = new System.Drawing.Point(20, 50);
+            this.txt_Valor1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_Valor1.Name = "txt_Valor1";
+            this.txt_Valor1.Size = new System.Drawing.Size(163, 32);
+            this.txt_Valor1.TabIndex = 0;
+            this.txt_Valor1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_clear.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clear.Location = new System.Drawing.Point(68, 250);
+            this.btn_clear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(80, 29);
+            this.btn_clear.TabIndex = 3;
+            this.btn_clear.Text = "Limpar";
+            this.btn_clear.UseVisualStyleBackColor = false;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // btn_Calc
+            // 
+            this.btn_Calc.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Calc.Location = new System.Drawing.Point(92, 162);
+            this.btn_Calc.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_Calc.Name = "btn_Calc";
+            this.btn_Calc.Size = new System.Drawing.Size(76, 29);
+            this.btn_Calc.TabIndex = 4;
+            this.btn_Calc.Text = "Calcular";
+            this.btn_Calc.UseVisualStyleBackColor = true;
+            this.btn_Calc.Click += new System.EventHandler(this.btn_Calc_Click);
+            // 
+            // rbtn3
+            // 
+            this.rbtn3.AutoSize = true;
+            this.rbtn3.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtn3.Location = new System.Drawing.Point(23, 120);
+            this.rbtn3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rbtn3.Name = "rbtn3";
+            this.rbtn3.Size = new System.Drawing.Size(101, 24);
+            this.rbtn3.TabIndex = 8;
+            this.rbtn3.TabStop = true;
+            this.rbtn3.Text = "Kilômetros";
+            this.rbtn3.UseVisualStyleBackColor = true;
+            this.rbtn3.CheckedChanged += new System.EventHandler(this.rbtn3_CheckedChanged);
+            // 
+            // rbtn2
+            // 
+            this.rbtn2.AutoSize = true;
+            this.rbtn2.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtn2.Location = new System.Drawing.Point(23, 92);
+            this.rbtn2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rbtn2.Name = "rbtn2";
+            this.rbtn2.Size = new System.Drawing.Size(78, 24);
+            this.rbtn2.TabIndex = 7;
+            this.rbtn2.TabStop = true;
+            this.rbtn2.Text = "Metros";
+            this.rbtn2.UseVisualStyleBackColor = true;
+            this.rbtn2.CheckedChanged += new System.EventHandler(this.rbtn2_CheckedChanged);
+            // 
+            // rbtn1
+            // 
+            this.rbtn1.AutoSize = true;
+            this.rbtn1.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtn1.Location = new System.Drawing.Point(23, 64);
+            this.rbtn1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rbtn1.Name = "rbtn1";
+            this.rbtn1.Size = new System.Drawing.Size(115, 24);
+            this.rbtn1.TabIndex = 6;
+            this.rbtn1.TabStop = true;
+            this.rbtn1.Text = "Centímetros";
+            this.rbtn1.UseVisualStyleBackColor = true;
+            this.rbtn1.CheckedChanged += new System.EventHandler(this.rbtn1_CheckedChanged);
+            // 
+            // gBox2
+            // 
+            this.gBox2.BackColor = System.Drawing.Color.White;
+            this.gBox2.Controls.Add(this.btn_Calc);
+            this.gBox2.Controls.Add(this.label3);
+            this.gBox2.Controls.Add(this.rbtn6);
+            this.gBox2.Controls.Add(this.rbtn5);
+            this.gBox2.Controls.Add(this.rbtn4);
+            this.gBox2.Controls.Add(this.label2);
+            this.gBox2.Controls.Add(this.rbtn3);
+            this.gBox2.Controls.Add(this.rbtn2);
+            this.gBox2.Controls.Add(this.rbtn1);
+            this.gBox2.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gBox2.Location = new System.Drawing.Point(247, 28);
+            this.gBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gBox2.Name = "gBox2";
+            this.gBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gBox2.Size = new System.Drawing.Size(320, 210);
+            this.gBox2.TabIndex = 1;
+            this.gBox2.TabStop = false;
+            this.gBox2.Text = "Escolha a Conversão";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(134, 33);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Para:";
+            // 
+            // rbtn6
+            // 
+            this.rbtn6.AutoSize = true;
+            this.rbtn6.Location = new System.Drawing.Point(137, 120);
+            this.rbtn6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rbtn6.Name = "rbtn6";
+            this.rbtn6.Size = new System.Drawing.Size(101, 24);
+            this.rbtn6.TabIndex = 12;
+            this.rbtn6.TabStop = true;
+            this.rbtn6.Text = "Kilômetros";
+            this.rbtn6.UseVisualStyleBackColor = true;
+            this.rbtn6.CheckedChanged += new System.EventHandler(this.rbtn6_CheckedChanged);
+            // 
+            // rbtn5
+            // 
+            this.rbtn5.AutoSize = true;
+            this.rbtn5.Location = new System.Drawing.Point(137, 92);
+            this.rbtn5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rbtn5.Name = "rbtn5";
+            this.rbtn5.Size = new System.Drawing.Size(78, 24);
+            this.rbtn5.TabIndex = 11;
+            this.rbtn5.TabStop = true;
+            this.rbtn5.Text = "Metros";
+            this.rbtn5.UseVisualStyleBackColor = true;
+            this.rbtn5.CheckedChanged += new System.EventHandler(this.rbtn5_CheckedChanged);
+            // 
+            // rbtn4
+            // 
+            this.rbtn4.AutoSize = true;
+            this.rbtn4.Location = new System.Drawing.Point(137, 64);
+            this.rbtn4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rbtn4.Name = "rbtn4";
+            this.rbtn4.Size = new System.Drawing.Size(115, 24);
+            this.rbtn4.TabIndex = 10;
+            this.rbtn4.TabStop = true;
+            this.rbtn4.Text = "Centímetros";
+            this.rbtn4.UseVisualStyleBackColor = true;
+            this.rbtn4.CheckedChanged += new System.EventHandler(this.rbtn4_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 33);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "De:";
+            // 
+            // Calculador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BackgroundImage = global::exercicioMedidas.Properties.Resources.sky_wallpapers_28043_8667192;
+            this.ClientSize = new System.Drawing.Size(577, 299);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.gBox2);
+            this.Controls.Add(this.gBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.Name = "Calculador";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calcula Metros";
+            this.gBox1.ResumeLayout(false);
+            this.gBox1.PerformLayout();
+            this.gBox2.ResumeLayout(false);
+            this.gBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gBox1;
+        private System.Windows.Forms.TextBox txt_res;
+        private System.Windows.Forms.TextBox txt_Valor1;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_Calc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbtn3;
+        private System.Windows.Forms.RadioButton rbtn2;
+        private System.Windows.Forms.RadioButton rbtn1;
+        private System.Windows.Forms.GroupBox gBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rbtn6;
+        private System.Windows.Forms.RadioButton rbtn5;
+        private System.Windows.Forms.RadioButton rbtn4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ exercicioMedidas/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exercicioMedidas
+{
+    public partial class Calculador : Form
+    {
+        public Calculador()
+        {
+            InitializeComponent();
+        }
+
+        int op = 0;
+        int op2 = 0;
+        string caution;
+        double v1;
+        double res;
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            txt_Valor1.Text = "";        
+            txt_res.Text = "";
+            op = 0;
+            op2 = 0;
+        }
+
+        private void btn_Calc_Click(object sender, EventArgs e)
+        {
+            if(op == 1 && op2 == 1)
+            {
+                v1 = Convert.ToInt32(txt_Valor1.Text);
+                res = v1;
+                txt_res.Text= Convert.ToString(res+" Centímetros");
+                op = 0;
+                op2 = 0;
+            }
+            if (op == 1 && op2 == 2)
+            {
+                v1 = Convert.ToInt32(txt_Valor1.Text);
+                res = v1 / 100;
+                txt_res.Text = Convert.ToString(res + " Metros");
+                op = 0;
+                op2 = 0;
+            }
+            if (op == 1 && op2 == 3)
+            {
+                v1 = Convert.ToInt32(txt_Valor1.Text);
+                res = v1 / 100000;
+                txt_res.Text = Convert.ToString(res + " Kilômetros");
+                op = 0;
+                op2 = 0;
+            }
+            if (op == 2 && op2 == 1)
+            {
+                v1 = Convert.ToInt32(txt_Valor1.Text);
+                res = v1 * 100;
+                txt_res.Text = Convert.ToString(res + " Centímetros");
+                op = 0;
+                op2 = 0;
+            }
+            if (op == 2 && op2 == 2)
+            {
+                v1 = Convert.ToInt32(txt_Valor1.Text);
+                res = v1;
+                txt_res.Text = Convert.ToString(res + " Metros");
+                op = 0;
+                op2 = 0;
+            }
+            if (op == 2 && op2 == 3)
+            {
+                v1 = Convert.ToInt32(txt_Valor1.Text);
+                res = v1 / 1000;
+                txt_res.Text = Convert.ToString(res + " Kilômetros");
+                op = 0;
+                op2 = 0;
+            }
+            if (op == 3 && op2 == 1)
+            {
+                v1 = Convert.ToInt32(txt_Valor1.Text);
+                res = v1 * 100000;
+                txt_res.Text = Convert.ToString(res + " Centímetros");
+                op = 0;
+                op2 = 0;
+            }
+            if (op == 3 && op2 == 2)
+            {
+                v1 = Convert.ToInt32(txt_Valor1.Text);
+                res = v1 * 1000;
+                txt_res.Text = Convert.ToString(res + " Metros");
+                op = 0;
+                op2 = 0;
+            }
+            if (op == 3 && op2 == 3)
+            {
+                v1 = Convert.ToInt32(txt_Valor1.Text);
+                res = v1;
+                txt_res.Text = Convert.ToString(res + " Kilômetros");
+                op = 0;
+                op2 = 0;
+            }
+        }
+
+        private void rbtn1_CheckedChanged(object sender, EventArgs e)
+        {
+            op = 1;
+          
+        }
+
+        private void rbtn2_CheckedChanged(object sender, EventArgs e)
+        {
+            op = 2;
+          
+        }
+
+        private void rbtn3_CheckedChanged(object sender, EventArgs e)
+        {
+            op = 3;
+            
+        }
+
+        private void rbtn4_CheckedChanged(object sender, EventArgs e)
+        {
+            op2 = 1;
+        }
+
+        private void rbtn5_CheckedChanged(object sender, EventArgs e)
+        {
+            op2 = 2;
+        }
+
+        private void rbtn6_CheckedChanged(object sender, EventArgs e)
+        {
+            op2 = 3;
+        }
+    }
+}
